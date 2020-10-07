85add8e99ea2fec668e94174674c520a6640cd56
EvanQube <evanenev@outlook.com>
2020-10-08 00:45:48 +0300

    1

--- index.js
+++ index.js
@@ -12,23 +12,3 @@ bot.on('message', msg => {
         msg.channel.send('pong');
     }
 })
-
-bot.on('message', msg => {
-    if(msg.content === prefix + 'help') {
-        const helpembed = new Discord.MessageEmbed()
-        .setColor('BLUE')
-        .setAuthor('Among Us по-русски', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.gg/C44mCXv')
-        .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-        .setTitle('Помощь')
-        .setDescription('Список доступных команд')
-        .addFields(
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Музыкальный бот', value: 'Его команды', inline: true },
-		{ name: 'JuniperBot', value: 'Его команды', inline: true },
-    )
-    .setImage('https://i.imgur.com/wSTFkRM.png')
-    .setTimestamp()
-    .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-    msg.channel.send(helpembed)
-    }
-})

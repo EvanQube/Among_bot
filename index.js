823cc0d0bc5048aeeaa26d6b0fbe0646fa773978
EvanQube <evanenev@outlook.com>
2020-10-08 12:04:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -41,13 +41,11 @@ client.on('message', msg => {
 const args = message.content.slice(prefix.length).trim().split(' ');
 const command = args.shift().toLowerCase();
 
-else {
  if (command === 'args-info') {
 	if (!args.length) {
 		return message.channel.send(`Вы не написали аргументы, ${message.author}!`);
 	}
 
 	message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-}
 }
 })

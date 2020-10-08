d31b1e5fe2e0988773ab4e6a9081bcdfc51afba2
EvanQube <evanenev@outlook.com>
2020-10-08 12:23:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const { prefix, token } = require('.config.json');
+const { prefix, token } = require('config.json');
 client.login(token);
 
 client.on('ready', () => {

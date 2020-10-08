f5c602e049b7945b5bb31597fd3e1570ceb0af74
EvanQube <evanenev@outlook.com>
2020-10-08 22:24:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -94,5 +94,6 @@ client.on('message', msg => {
 .setImage('https://i.imgur.com/wSTFkRM.png')
 .setTimestamp()
 .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+msg.channel.send(repmebed)
   }
  });

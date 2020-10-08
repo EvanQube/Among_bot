e63ba21bf961c30b8d4db9348f1357e3643c89b2
EvanQube <evanenev@outlook.com>
2020-10-08 12:27:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,3 +32,20 @@ client.on('message', msg => {
     msg.channel.send(helpembed)
     }
 })
+
+
+client.on('message', msg => {
+
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+const args = message.content.slice(prefix.length).trim().split(' ');
+const command = args.shift().toLowerCase();
+
+ if (command === 'args-info') {
+	if (!args.length) {
+		return message.channel.send(`Вы не написали аргументы, ${message.author}!`);
+	}
+
+	message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+}
+})

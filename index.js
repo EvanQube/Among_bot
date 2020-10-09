9ef773bf6cd8ab29a140e145c002be7dc4291f0b
EvanQube <evanenev@outlook.com>
2020-10-09 12:59:40 +0300

    Обновление рандома

--- index.js
+++ index.js
@@ -71,9 +71,9 @@ client.on('message', message => {
 });
 
 client.on('message', msg =>{
-  if(msg.startsWith(prefix + 'rand'))
+  if(msg.content === prefix + 'rand')) {
   number = 3;
   let imageNumber = Math.floor(Math.random() *(number - 1 + 1)) + 1;
   msg.channel.send({files: ["./images/" + imageNumber + ".png"]})
-
+}
 });

59b9dbc6a205bf0ac215740375744c5ac50b1f5d
EvanQube <evanenev@outlook.com>
2020-10-09 13:57:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ client.on('message', message => {
 });
 
 client.on('message', msg =>{
-  if(msg.content === prefix + 'rand')) {
+  if(msg.content === prefix + 'rand') {
   number = 3;
   let imageNumber = Math.floor(Math.random() *(number - 1 + 1)) + 1;
   msg.channel.send({files: ["./images/" + imageNumber + ".png"]})

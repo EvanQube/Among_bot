4782a72735070b2c17a16d3cf437d38c388e4b8d
EvanQube <evanenev@outlook.com>
2020-10-12 02:21:51 +0300

    Minor

--- commands/nick.js
+++ commands/nick.js
@@ -1,9 +1,3 @@
-if (message.content.includes('changeNick')) {
-    if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.channel.send('I don\'t have permission to change your nickname!');
-    message.member.setNickname(message.content.replace('changeNick ', ''));
-}
-
-
 module.exports = {
 	name: 'nick',
 	description: 'Change nickname',

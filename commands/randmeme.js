af5b4cf8b4dcdefc58ee8868aa16526223e7809a
EvanQube <evanenev@outlook.com>
2020-10-12 16:40:14 +0300

    Изменение расширения

--- commands/randmeme.js
+++ commands/randmeme.js
@@ -4,6 +4,6 @@ module.exports = {
 	execute(message, args) {
 		number = 11;
     imageNumber = Math.floor(Math.random() * (number - 1)) + 1;
-    message.channel.send({files: ['./images/' + imageNumber + '.png']})
+    message.channel.send({files: ['./images/' + imageNumber + '.jpg']})
 	},
 };

84fa4b254a55e64a9b57cbeb33474cc2cde7229c
EvanQube <evanenev@outlook.com>
2020-10-12 12:35:56 +0300

    Minor

--- commands/nick.js
+++ commands/nick.js
@@ -1,22 +1,9 @@
-module.exports.help = {
-  name: "nick",
-  description: "Set a user nickname.",
-}
-
-module.exports.run = async (client, message, args) => {
-
-  if(!message.member.roles.cache.get('759402969803390997') || !message.member.roles.cache.get('756567959526309961') || !message.member.roles.cache.get('759399027661209610') || !message.member.roles.cache.get('761188475755167794') || !message.member.roles.cache.get('756567959526309962')) {
-    return message.channel.send({embed: {color: "RED", description: "You can't use this command!"}})
-  }
-
-  let user = message.mentions.users.first();
-  if (!user) return message.channel.send({embed: {color: "RED", description: "You need to mention the user!"}});
-
-  let nick = args.slice(1).join(" ");
-  if (!nick) return message.channel.send({embed: {color: "RED", description: "You need to input the nickname!"}});
-
-  let member = message.guild.members.cache.get(user.id);
-
-  await member.setNickname(nick).catch(err => message.channel.send({embed: {color: "RED", description: `Error: ${err}`}}));
-  return message.channel.send({embed: {color: "GREEN", description: `Successfully changed **${user.tag}** nickname to **${nick}**`}});
-}
+module.exports = {
+	name: 'nick',
+	description: 'Change nickname',
+	execute(client, message, args) {
+    if(!message.member.roles.cache.get('756567959526309961') || !message.member.roles.cache.get('759399027661209610') || !message.member.roles.cache.get('759399027661209610') || !message.member.roles.cache.get('761188475755167794')) return;
+    if(!args[0]) return message.channel.send('**Вы должны упомянуть человека, которому хотите изменить ник**')
+    message.guild.members.get(message.mentions.users.first()).setNickname(args);
+	},
+};

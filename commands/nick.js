672496f3d480ce2c963d11f50a260858de7dae82
EvanQube <evanenev@outlook.com>
2020-10-12 11:59:29 +0300

    Minor

--- commands/nick.js
+++ commands/nick.js
@@ -17,7 +17,7 @@ exports.run = async (client, message, args) => {
 }
 
 exports.help = {
-  name: "setnickname",
+  name: "nick",
   description: "Set a user nickname.",
   usage: "/setnickname <@user> <nick>",
   example: "/setnickname @ray#9999 hoisted"

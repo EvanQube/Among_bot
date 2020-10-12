0bcb18dad2d953878aa073e16d452bf650d01ff5
EvanQube <evanenev@outlook.com>
2020-10-12 22:15:10 +0300

    Minor

--- member-count.js
+++ member-count.js
@@ -1,12 +1,10 @@
 module.exports = client => {
 
-  const updateMembers = (guild) => {
   const channel = guild.channels.cache.get('765263675434074123')
   channel.setName(`Участников: ${guild.memberCount.toLocaleString()}`)
-}
 
-client.on('guildMemberAdd', (member) => updateMembers(member.guild))
-client.on('guildMemberRemove', (member) => updateMembers(member.guild))
+client.on('guildMemberAdd', (member) => {channel.setName(`Участников: ${guild.memberCount.toLocaleString()}`)})
+client.on('guildMemberRemove', (member) => {channel.setName(`Участников: ${guild.memberCount.toLocaleString()}`)})
 
 const guild = client.guilds.cache.get('756567959526309958')
 updateMembers(guild)

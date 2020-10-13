f44d42d54b89388a74d9f483a705698a71226297
EvanQube <evanenev@outlook.com>
2020-10-13 11:45:07 +0300

    Minor

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ client.on('message', msg =>{
     }
 
     else if(command === 'helper') { //набор на хелперов
-    client.guild.channels.cache.get('765494369473003531').send(args);
+    msg.guild.channels.cache.get('765494369473003531').send(args);
     msg.delete().catch();
   }
 

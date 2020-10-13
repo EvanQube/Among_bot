adfc567893de9bc24a63a5f36da816a204dcd1bc
EvanQube <evanenev@outlook.com>
2020-10-13 18:29:31 +0300

    Minor

--- index.js
+++ index.js
@@ -181,3 +181,10 @@ else {
 }
 }
 });
+
+
+client.on('message', (msg) => {
+  if(msg.channels.cache.get('761622039408410644')) {
+    msg.guild.channels.cache.get('765494369473003531').send(msg.content);
+  }
+});

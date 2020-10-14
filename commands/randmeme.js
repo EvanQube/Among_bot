f1bb8bbe6ad3721285669ebf0536dd79a6b74bae
EvanQube <evanenev@outlook.com>
2020-10-14 14:23:27 +0300

    Удаление одного мема

--- commands/randmeme.js
+++ commands/randmeme.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'randmeme',
 	description: 'Random meme',
 	execute(msg, args) {
-		number = 11;
+		number = 10;
     imageNumber = Math.floor(Math.random() * (number - 1)) + 1;
     msg.channel.send({files: ['./images/' + imageNumber + '.jpg']})
     msg.delete().catch();

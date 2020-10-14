62f667861f9be6e1c26b005aaf7cdadb9d105371
EvanQube <evanenev@outlook.com>
2020-10-14 11:56:20 +0300

    Minor

--- commands/hug.js
+++ commands/hug.js
@@ -3,9 +3,9 @@ module.exports = {
 	description: 'Hugs',
 	execute(msg, args) {
     user = msg.mentions.users.first();
-    author = msg.author();
+    author = msg.author.id;
     hugs = 3;
     hugsNumber = Math.floor(Math.random() * (hug - 1)) + 1;
-    msg.channel.send('<@' + user.id + '>' + '<@' + author.id + '>' + {files: ['./hugs/' + hugsNumber + '.jpg']})
+    msg.channel.send('<@' + user.id + '>' + '<@' + author + '>' + {files: ['./hugs/' + hugsNumber + '.jpg']})
 	},
 };

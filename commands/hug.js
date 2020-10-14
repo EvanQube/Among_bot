e41abf65af12f3632eb179e376ecff1fa51f67a4
EvanQube <evanenev@outlook.com>
2020-10-14 13:00:36 +0300

    Minor

--- commands/hug.js
+++ commands/hug.js
@@ -5,12 +5,13 @@ module.exports = {
     if(!msg.mentions.users.size) {
       msg.reply('Нужно отметить пользователся, которого вы хотите обнять.')
     }
+    else {
     user = msg.mentions.users.first();
     author = msg.author.id;
     hugs = 12;
     hugsNumber = Math.floor(Math.random() * (hugs - 1)) + 1;
     msg.channel.send('<@' + author + '>' + '\xa0' + 'обнимает' + '\xa0' + '<@' + user.id + '>', {files: ['./hugs/' + hugsNumber + '.gif']})
     msg.delete().catch();
-
+  }
   },
 };

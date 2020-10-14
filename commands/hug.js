55464690d8ac20adb5969d25bcdf0475c093696e
EvanQube <evanenev@outlook.com>
2020-10-14 13:00:02 +0300

    Minor

--- commands/hug.js
+++ commands/hug.js
@@ -2,6 +2,9 @@ module.exports = {
 	name: 'hug',
 	description: 'Hugs',
 	execute(msg, args) {
+    if(!msg.mentions.users.size) {
+      msg.reply('Нужно отметить пользователся, которого вы хотите обнять.')
+    }
     user = msg.mentions.users.first();
     author = msg.author.id;
     hugs = 12;
@@ -9,8 +12,5 @@ module.exports = {
     msg.channel.send('<@' + author + '>' + '\xa0' + 'обнимает' + '\xa0' + '<@' + user.id + '>', {files: ['./hugs/' + hugsNumber + '.gif']})
     msg.delete().catch();
 
-    if(!msg.mentions.users.size) {
-      msg.reply('Нужно отметить пользователся, которого вы хотите обнять.')
-    }
   },
 };

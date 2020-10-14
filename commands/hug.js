bcc9102ad66ff2799768b4d8ce38842499a50cb2
EvanQube <evanenev@outlook.com>
2020-10-14 12:10:33 +0300

    Добавление команды hug

--- commands/hug.js
+++ commands/hug.js
@@ -4,8 +4,8 @@ module.exports = {
 	execute(msg, args) {
     user = msg.mentions.users.first();
     author = msg.author.id;
-    hugs = 3;
+    hugs = 2;
     hugsNumber = Math.floor(Math.random() * (hugs - 1)) + 1;
-    msg.channel.send('<@' + user.id + '>' + '<@' + author + '>' + {files: ['./hugs/' + hugsNumber + '.jpg']})
+    msg.channel.send('<@' + author + '>''<@' + '\xa0' + user.id + '>' + '\xa0' + {files: ['./hugs/' + hugsNumber + '.gif']})
 	},
 };

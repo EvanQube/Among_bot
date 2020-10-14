c3dcd58683a43d7c0e5c3f048906837ff098b8ef
EvanQube <evanenev@outlook.com>
2020-10-14 12:45:16 +0300

    Minor

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ client.on('message', msg =>{
     else if(command === 'hug') {
             client.commands.get('hug').execute(msg, args);
           }
-          
+
     else if(command === 'help') {
       client.commands.get('help').execute(msg, client);
     }
@@ -125,32 +125,6 @@ client.on('guildMemberRemove', member => {
     membercountchannel.setName(`Участников:` + member.guild.memberCount)
 
 })
-
-
-client.on('message', msg =>{
-    if(!msg.content.startsWith(prefix) || msg.author.bot) return;
-    if(msg.content === prefix + 'help') {
-    const helpembed = new Discord.MessageEmbed()
-    .setColor('ORANGE')
-.setAuthor('Among Us по-русски', 'https://i.imgur.com/Tc6QKK1.jpg', 'https://discord.gg/C44mCXv')
-.setTitle('Помощь')
-.setDescription('Список доступных команд')
-.addFields(
-{ name: '\u200B', value: '\u200B' },
-{ name: 'Фан', value:
-prefix + 'rand - скидывает случайную картинку по Among Us _(временно не работает)_' + "\n" +
-prefix + 'among - проверка работоспособности бота'+ "\n" +
-prefix + 'randmeme - скидывает рандомный мем, связванный с сервером' + "\n" +
-prefix + 'idea - есть идеи для бота ? Эта команда для тебя!'
-},
-)
-.setImage('https://i.imgur.com/AYlRRkt.png')
-.setTimestamp()
-.setFooter('Among Us по-русски', 'https://i.imgur.com/Tc6QKK1.jpg');
-msg.channel.send(helpembed)
-msg.delete().catch();
-}
-});
 
 
 client.on('message', msg =>{

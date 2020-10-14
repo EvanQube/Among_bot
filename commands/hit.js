c9e6d2648cfaa14763e9e79f95835f402b5a009f
EvanQube <evanenev@outlook.com>
2020-10-14 13:29:02 +0300

    Обновление числа

--- commands/hit.js
+++ commands/hit.js
@@ -8,7 +8,7 @@ module.exports = {
     else {
     user = msg.mentions.users.first();
     author = msg.author.id;
-    hits = 1;
+    hits = 13;
     hitsNumber = Math.floor(Math.random() * (hits - 1)) + 1;
     msg.channel.send('<@' + author + '>' + '\xa0' + 'ударил' + '\xa0' + '<@' + user.id + '>', {files: ['./hits/' + hitsNumber + '.gif']})
     msg.delete().catch();

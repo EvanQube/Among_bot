d29da82ec234e5dddab38c5172e5a1f911413305
EvanQube <evanenev@outlook.com>
2020-10-14 14:15:13 +0300

    Minor

--- commands/cut.js
+++ commands/cut.js
@@ -8,7 +8,9 @@ module.exports = {
     else {
     user = msg.mentions.users.first();
     author = msg.author.id;
-    msg.channel.send('<@' + author + '>' + '\xa0' + 'отрезал' + '\xa0' + '<@' + user.id + '>' + '\xa0' + 'пипиську', {files: ['cut.jpeg']})
+    cuts = 4;
+    cutsNumber = Math.floor(Math.random() * (cuts - 1)) + 1;
+    msg.channel.send('<@' + author + '>' + '\xa0' + 'отрезал' + '\xa0' + '<@' + user.id + '>' + '\xa0' + 'пипиську', {files: ['./cuts/' 4 cutsNumber + '.gif']})
     msg.delete().catch();
   }
   },

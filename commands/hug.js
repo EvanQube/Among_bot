f7f8f923ba1752ccd51225461202ad6c28eaf7f0
EvanQube <evanenev@outlook.com>
2020-10-14 12:22:46 +0300

    Добавление гиифок

--- commands/hug.js
+++ commands/hug.js
@@ -4,8 +4,9 @@ module.exports = {
 	execute(msg, args) {
     user = msg.mentions.users.first();
     author = msg.author.id;
-    hugs = 2;
+    hugs = 4;
     hugsNumber = Math.floor(Math.random() * (hugs - 1)) + 1;
     msg.channel.send('<@' + author + '>' + '\xa0' + 'обнимает' + '\xa0' + '<@' + user.id + '>', {files: ['./hugs/' + hugsNumber + '.gif']})
-	},
+    msg.delete().catch();
+  },
 };

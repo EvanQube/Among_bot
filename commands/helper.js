da7640f9bc613d438aa647d1e96fdd3dde50be91
EvanQube <evanenev@outlook.com>
2020-10-14 19:50:00 +0300

    Обноваление команды >helper

--- commands/helper.js
+++ commands/helper.js
@@ -1,17 +1,17 @@
 module.exports = {
 	name: 'helper',
-	description: 'Заявка на хелпера',
+	description: 'Жалоба хелперу',
 	execute(msg, args, assist, tech) {
 		//if(!assist || !tech || !admin) return msg.reply('у вас нет прав использовать эту команду');
-		if(!args[1]) return msg.reply('необходимо пингануть нарушителя');
-		if(!args[2]) return msg.reply('необходимо указать нарушение');
+		if(!args[0]) return msg.reply('необходимо пингануть нарушителя');
+		if(!args[1]) return msg.reply('необходимо указать нарушение');
 		const channelId = msg.channel.id;
 		const author = msg.author.id;
 		const helperChannel = msg.guild.channels.cache.get('765598395905605642');
 		helperChannel.send(
 			'<@' + author + '>' + '\n' +
+			args[0] + '\n' +
 			args[1] + '\n' +
-			args[2] + '\n' +
 			'<#' + channelId + '>'
 		)
     msg.delete().catch();

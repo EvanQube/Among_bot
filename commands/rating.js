ace27387f33980c6d270559fd65d87fc7dc3376a
EvanQube <evanenev@outlook.com>
2020-10-15 21:39:16 +0300

    Minor

--- commands/rating.js
+++ commands/rating.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'rating',
   description: 'Включение режима игр на рейтинг',
   execute(msg, args, ) {
-    const channel = msg.guild.channel.cache.get('760540460346638386');
+    const channel = msg.guild.channels.cache.get('760540460346638386');
     const admin = msg.member.hasPermission("ADMINISTRATOR");
     if (!msg.member.roles.cache.get('764864882390204456') || !admin) return msg.reply('**у вас нет прав на использование этой команды!**');
     if (!args[0]) return msg.reply('необходимо указать on или off')

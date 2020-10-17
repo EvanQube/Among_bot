4e96c772730439fa5bc03facbd14a2d231e0da17
EvanQube <evanenev@outlook.com>
2020-10-17 23:13:37 +0300

    Minor

--- commands/halloween2020.js
+++ commands/halloween2020.js
@@ -17,7 +17,7 @@ module.exports = {
       .setImage('https://freedesignfile.com/upload/2014/10/Vector-set-of-halloween-pumpkin-design-material-05.jpg')
       .setTimestamp()
       .setFooter('Among Us по-русски', 'https://i.imgur.com/Tc6QKK1.jpg');
-    msg.channel.send(halloween2020Embed + '<@here>')
+    msg.channel.send('@here', halloween2020Embed)
     msg.delete().catch();
   },
 };
